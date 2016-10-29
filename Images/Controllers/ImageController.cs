57ea23f780fa203753794ddbb3f9694e5f3d793a
Daniel Smith <danny@stormpoopersmith.com>
2016-10-29 18:13:39 +0100

    Read image from Base64 for create, fallback to standard image

--- Images/Controllers/ImageController.cs
+++ Images/Controllers/ImageController.cs
@@ -16,8 +16,16 @@ namespace Images.Controllers
         [HttpPost, Route("api/image/create")]
         public IHttpActionResult Create(MessageDetails details)
         {
-            var imagePath = System.Web.Hosting.HostingEnvironment.MapPath(@"~/App_Data/espionage.jpg");
-            var bmp = new Bitmap(Image.FromFile(imagePath));
+            Bitmap bmp;
+            if (details.Image != null)
+            {
+                bmp = GetBitmapFromBase64(details.Image);
+            }
+            else
+            {
+                var imagePath = System.Web.Hosting.HostingEnvironment.MapPath(@"~/App_Data/espionage.jpg");
+                bmp = new Bitmap(Image.FromFile(imagePath));
+            }
             var img = Steganography.Embed(details.Target + ":" + details.Message, bmp);
             var ms = new MemoryStream();
             img.Save(ms, ImageFormat.Png);
@@ -28,11 +36,17 @@ namespace Images.Controllers
         [HttpPost, Route("api/image/retrieve")]
         public IHttpActionResult Retrieve(CreatedImage details)
         {
-            var bytes = Convert.FromBase64String(details.Image);
-            var ms = new MemoryStream(bytes);
-            var bmp = new Bitmap(Image.FromStream(ms));
+            var bmp = GetBitmapFromBase64(details.Image);
             var message = Steganography.Extract(bmp);
             return Ok(new RetrievedMessage { Message = message });
+        }
+
+        private static Bitmap GetBitmapFromBase64(string image)
+        {
+            var bytes = Convert.FromBase64String(image);
+            var ms = new MemoryStream(bytes);
+            var bmp = new Bitmap(Image.FromStream(ms));
+            return bmp;
         }
     }
 }
--- Images/Models/MessageDetails.cs
+++ Images/Models/MessageDetails.cs
@@ -2,6 +2,7 @@
 {
     public class MessageDetails
     {
+        public string Image { get; set; }
         public string Target { get; set; }
         public string Message { get; set; }
     }
