20990d4a0088724b6c39829c88dd971d2b57412b
bob poole <bob.poole@brighthr.com>
2016-10-29 22:23:54 +0100

    Now only decrypts images within location

--- Images/Controllers/TwitterController.cs
+++ Images/Controllers/TwitterController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Device.Location;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
@@ -8,6 +9,7 @@ using System.Net;
 using System.Threading;
 using System.Web.Http;
 using System.Web.Http.Controllers;
+using System.Web.Script.Serialization;
 using Images.Models;
 using TweetSharp;
 
@@ -72,23 +74,34 @@ namespace Images.Controllers
                 {
                     if (media.MediaType == TwitterMediaType.Photo)
                     {
-                        using (WebClient client = new WebClient())
+                        using (var client = new WebClient())
                         {
                             var image = client.DownloadData(media.MediaUrl);
                             var ms = new MemoryStream(image);
                             var bmp = new Bitmap(Image.FromStream(ms));
 
                             var decryptedMessage = Steganography.Extract(bmp);
-                            encrypted = true;
-                            statuses.Add(new Tweets
+                            var embedded = new JavaScriptSerializer().Deserialize<EmbeddedDetails>(decryptedMessage);
+                            
+                            var currentLocation = new GeoCoordinate {Latitude = latitude, Longitude = longitude};
+                            var requiredLocation = new GeoCoordinate
                             {
-                                Text = twitterStatus.Text,
-                                ScreenName = twitterStatus.User.ScreenName,
-                                Name = twitterStatus.User.Name,
-                                MediaUrls = urls,
-                                ProfileImageUrl = twitterStatus.User.ProfileImageUrl,
-                                decryptedMessage = decryptedMessage
-                            });
+                                Latitude = embedded.Latitude,
+                                Longitude = embedded.Longitude
+                            };
+                            if (GeoLocation.WithinRadius(currentLocation, requiredLocation))
+                            {
+                                encrypted = true;
+                                statuses.Add(new Tweets
+                                {
+                                    Text = twitterStatus.Text,
+                                    ScreenName = twitterStatus.User.ScreenName,
+                                    Name = twitterStatus.User.Name,
+                                    MediaUrls = urls,
+                                    ProfileImageUrl = twitterStatus.User.ProfileImageUrl,
+                                    decryptedMessage = decryptedMessage
+                                });
+                            }
 
                         }
                     }
@@ -102,20 +115,9 @@ namespace Images.Controllers
                     MediaUrls = urls,
                     ProfileImageUrl = twitterStatus.User.ProfileImageUrl
                 });
-
                }
 
             return Ok(response);
         }
-    }
-
-    public class Tweets
-    {
-        public string ScreenName { get; set; }
-        public string Name { get; set; }
-        public string Text { get; set; }
-        public string ProfileImageUrl { get; set; }
-        public List<string> MediaUrls { get; set; }
-        public string decryptedMessage { get; set; }
     }
 }
