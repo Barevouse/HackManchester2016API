f8980c29e13f2dbd5fdad6ffe4df3f2ddc314b25
Daniel Smith <danny@stormpoopersmith.com>
2016-10-29 15:25:42 +0100

    Format as JSON

--- Images/App_Start/WebApiConfig.cs
+++ Images/App_Start/WebApiConfig.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using Newtonsoft.Json;
 
 namespace Images.App_Start
 {
@@ -13,6 +18,23 @@ namespace Images.App_Start
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.Add(new BrowserJsonFormatter());
+        }
+    }
+
+    public class BrowserJsonFormatter : JsonMediaTypeFormatter
+    {
+        public BrowserJsonFormatter()
+        {
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            this.SerializerSettings.Formatting = Formatting.Indented;
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("application/json");
         }
     }
 }
